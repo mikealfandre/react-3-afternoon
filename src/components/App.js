fd31ec84af9c74a10563a8ce5b962ea20e2d12b7
Michael Alfandre <Michael Alfandre>
2019-01-16 16:28:16 -0700

    completed react-3-afternoon

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import Post from './Post/Post'
 
 import './App.css';
 
@@ -19,23 +21,52 @@ class App extends Component {
   }
   
   componentDidMount() {
+    axios.get('https://practiceapi.devmountain.com/api/posts')
+    .then(response => {
+      this.setState({
+        posts: response.data
+      })
+    })
 
   }
 
-  updatePost() {
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text})
+    .then((response) => {
+      this.setState({
+        posts: response.data
+      })
+    })
   
   }
 
-  deletePost() {
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+    
+    .then((response) => {
+      this.setState({
+        
+        posts: response.data
+      })
+      
+    })
 
   }
 
-  createPost() {
+  createPost(text) {
+    axios.post('https://practiceapi.devmountain.com/api/posts', {text})
+    .then((response) => {
+      this.setState ({
+        posts: response.data
+      })
+    })
 
   }
 
   render() {
     const { posts } = this.state;
+
+  
 
     return (
       <div className="App__parent">
@@ -43,7 +74,10 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
+          <Compose createPostFn={this.createPost} />
+          
+          {posts.map(post => (<Post key={post.id} text={post.text} date={post.date} updatePostFn={this.updatePost} id={post.id} deletePostFn={this.deletePost}/>))}
+          
           
         </section>
       </div>
--- src/components/Post/Edit/Edit.js
+++ src/components/Post/Edit/Edit.js
@@ -19,7 +19,12 @@ export default class Edit extends Component {
     this.setState({ text: value });
   }
 
-  updatePost() {
+  updatePost() { 
+    const {text} = this.state
+    const {updatePostFn, hideEdit, id} = this.props
+    
+    updatePostFn(id, text)
+    hideEdit()
 
   }
 
